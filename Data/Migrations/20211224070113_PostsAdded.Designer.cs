1b45f060806054e4cb1dfb0dc28da041e101360b
Artiom <artiom.tapunov@gmail.com>
2021-12-24 10:10:05 +0300

    PostsAdded migration

--- Data/Migrations/20211224070113_PostsAdded.Designer.cs
+++ Data/Migrations/20211224070113_PostsAdded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestAPI.Data;
 
 namespace TestAPI.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211224070113_PostsAdded")]
+    partial class PostsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
